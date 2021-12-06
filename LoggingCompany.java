cc9aed935db16500c41b716c1effabb4d7e3cede
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-06 17:33:46 +0000

    Updated comments

--- LoggingCompany.java
+++ LoggingCompany.java
@@ -10,13 +10,16 @@ import java.util.Scanner;
 
 // class LoggingCompany.
 class LoggingCompany {
-  // Main does the necessary calculations and outputs the number of logs the truck can carry.
+  // declare constants
+  final int logWeight = 20;
+  final int truckLimit = 1100;
+
   public static void main(String[] args) {
     System.out.println("What is the length of each log (0.25 metres, 0.50 metres, and 1 metres)?");
+    // Ask the user for the length of the log and reads it
     Scanner scannerObj = new Scanner(System.in);
     double logLength = scannerObj.nextDouble();
-    final int logWeight = 20;
-    final int truckLimit = 1100;
+    // Calculate and display the number of logs
     int logCount = (int) (truckLimit / (logWeight * logLength));
     System.out.printf("The number of logs of length %s and weight %s that can be carried is %s\n",
                         logLength, logWeight, logCount);
